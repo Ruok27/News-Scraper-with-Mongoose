a0371a78acfdbdf823a62e9338fa2373bf296064
ruok27 <jakxii27@gmail.com>
2020-02-07 02:11:15 -0500

    handlebars set up

--- server.js
+++ server.js
@@ -3,3 +3,32 @@ const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const axios = require("axios");
+
+
+
+let server = express();
+server.use(express.static("public"));
+
+let PORT = process.env.PORT || 3000;
+
+server.engine("handlebars", handlebars({defaultLayout: "main"}));
+server.set("view engine", "handlebars");
+
+server.get("/", function(req, res){
+
+res.render("index");
+
+
+})
+
+
+
+server.listen(PORT, function(){
+
+console.log(`Server listening on: http://localhost:${PORT}`)
+
+
+});
+
+
+
