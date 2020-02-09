64919fb1b27b3c1462174564fa392a3c95c5dc30
ruok27 <jakxii27@gmail.com>
2020-02-09 02:57:29 -0500

    did something

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+//Dependencies
 const express = require("express");
 const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const axios = require("axios");
+var logger = require("morgan");
+
+//Requires
+
+
+
+
+
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+
 
 
 
 let server = express();
 server.use(express.static("public"));
+
+
+mongoose.connect(MONGODB_URI);
 
 let PORT = process.env.PORT || 3000;
 
